0124477202aee4bb204e5b923b1dbfaff4a9eb2f
yennanliu <f339339@gmail.com>
2024-11-10 17:03:58 +0800

    update

--- dev_project/flask_https/app.py
+++ dev_project/flask_https/app.py
@@ -8,5 +8,8 @@ def home():
     return jsonify(message="Hello, HTTPS world!")
 
 if __name__ == '__main__':
-    app.run(ssl_context=('cert.pem', 'key.pem'))  # Configure for HTTPS
+    #app.run(ssl_context='adhoc') # can use ad-hoc ssl
+    #app.run(ssl_context=('cert.pem', 'key.pem'))  # Configure for HTTPS
     #app.run(ssl_context=(app.config['SSL_CERT_FILE'], app.config['SSL_KEY_FILE']))
+    context = ('cert.pem', 'key.pem')
+    app.run(ssl_context=context)
